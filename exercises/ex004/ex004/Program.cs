6aa5ecbebd22b1a1c7a516508f436f373001fa5b
Gonçalo Pinto <63504894+gpe0@users.noreply.github.com>
2020-04-18 21:17:10 +0100

    Add ex004

---
+++ exercises/ex004/ex004/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ex004
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int n1;
+            int n2;
+            Console.WriteLine("What is your name?");
+            name = Console.ReadLine();
+            Console.Write("Hello {0}, type a number: ", name);
+            n1 = Convert.ToInt32(Console.ReadLine()); //Converting the string value to Integer
+            Console.Write("Now type another number: ");
+            n2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0} + {1} = {2}", n1, n2, n1 + n2);
+            Console.WriteLine("{0} - {1} = {2}", n1, n2, n1 - n2);
+            Console.WriteLine("{0} x {1} = {2}", n1, n2, n1 * n2);
+            Console.WriteLine("{0} / {1} = {2}", n1, n2, n1 / n2); // The result will be a integer!!
+            Console.WriteLine("The reminder of {0} / {1} is {2}", n1, n2, n1 % n2);
+            n1++; //increment by 1
+            n2--; //decrement by 1
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+        }
+    }
+}
