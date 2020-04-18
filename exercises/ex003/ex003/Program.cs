b888519c8676c52f143ad74733659280fde2ec0c
Gonçalo Pinto <63504894+gpe0@users.noreply.github.com>
2020-04-18 21:07:01 +0100

    Add ex003

---
+++ exercises/ex003/ex003/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ex003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "Gonçalo"; //variables declared with var keyword must be initialized with a value
+            const double pi = 3.14; //const variables can't be changed
+            int start = 0;
+            int finish = 1;
+            Console.WriteLine(Convert.ToBoolean(start)); //Converting the start variable's value to Boolean
+            Console.WriteLine(Convert.ToBoolean(finish));
+        }
+    }
+}
