110ddffc1cf5bcb273d2de31cde4b5d59acb6c3f
Gonçalo Pinto <63504894+gpe0@users.noreply.github.com>
2020-04-18 21:06:43 +0100

    Add ex002

---
+++ exercises/ex002/ex002/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ex002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string yourName; //Creates a variable of string type
+            Console.WriteLine("What is your name?");
+            yourName = Console.ReadLine(); //Reads user's input
+            Console.WriteLine("Hello, {0}!", yourName); // Formatted string, 0 means the first element
+        }
+    }
+}
