db74d7c2fc2dd224f17e55d41277f85f672a6857
Gonçalo Pinto <63504894+gpe0@users.noreply.github.com>
2020-04-18 20:54:28 +0100

    Changing ex001 location

--- exercises/ex001/ex001/Program.cs
+++ exercises/ex001/ex001/Program.cs
@@ -6,7 +6,7 @@ namespace ex001
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!"); //Prints "Hello World!"
         }
     }
 }
