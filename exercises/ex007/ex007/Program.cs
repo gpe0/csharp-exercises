1215eda71d6211976216c061ce3d49727ee64d8a
Gonçalo Pinto <63504894+gpe0@users.noreply.github.com>
2020-04-19 19:09:30 +0100

    Add ex007

---
+++ exercises/ex007/ex007/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ex007
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int x = 1; x < 6; x++) // FOR Loop | loop for 5 times | starting in 1 and following the statement "x < 6"
+            {
+                Console.WriteLine("The value of x is : {0}", x);
+            }
+        }
+    }
+}
