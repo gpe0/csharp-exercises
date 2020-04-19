38f38adcea071ff6c78cfaa8b49618518856d615
Gonçalo Pinto <63504894+gpe0@users.noreply.github.com>
2020-04-19 19:24:54 +0100

    Add ex009

---
+++ exercises/ex009/ex009/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ex009
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 40;
+            do // similar to WHILE Loop, DO-WHILE Loop is guaranteed to execute at least one time.
+            {
+                Console.WriteLine(x++);
+            } while (x <= 10);
+            Console.WriteLine("-------------------");
+            int y = 1;
+            do
+            {
+                Console.WriteLine(y++);
+            } while (y <= 10);
+        }
+    }
+}
