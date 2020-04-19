e9ecddff934b9f66e3b3e36028cb72906ced6f06
Gonçalo Pinto <63504894+gpe0@users.noreply.github.com>
2020-04-19 19:20:04 +0100

    Add ex008

---
+++ exercises/ex008/ex008/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ex008
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+            while (true) // WHILE Loop | It is an infinite loop | Only stops when a break "break" occurs  
+            {
+                Console.Write(" {0}", x);
+                x++;
+                if (x == 11) 
+                {
+                    Console.Write(" Finishing!");
+                    break; // Stopping after looping for 10 times.
+                }
+            }
+        }
+    }
+}
