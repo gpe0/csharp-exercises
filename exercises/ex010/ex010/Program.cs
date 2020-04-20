5461b3a1e1ebef065c3c30d00333e0bcfd1689a5
Gonçalo Pinto <63504894+gpe0@users.noreply.github.com>
2020-04-20 15:12:23 +0100

    Add ex010

---
+++ exercises/ex010/ex010/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ex010
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 7;
+            if (x < 7 && y > 2) //Logical Operations | AND - &&
+            {
+                Console.WriteLine("Correct!");
+            }
+            else
+            {
+                Console.WriteLine("Wrong");
+            }
+            //Outputs "Wrong", because  the result of an AND operation is only true when both statements are true.
+        }
+    }
+}
