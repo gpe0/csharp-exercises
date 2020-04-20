aa542da87628b92c44ac5d6975aebc877b6cda68
Gonçalo Pinto <63504894+gpe0@users.noreply.github.com>
2020-04-20 15:35:10 +0100

    Add ex013

---
+++ exercises/ex013/ex013/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ex013
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            string msg;
+            Console.Write("Your age: ");
+            age = Convert.ToInt32(Console.ReadLine());
+            msg = age >= 18 ? "Welcome!" : "Sorry!"; // Conditional operator, the "?:" operator.
+            Console.WriteLine(msg); //If the statement is true, the output is "Welcome!"
+                                    //If the statement is false, the output is "Sorry!"
+        }
+    }
+}
