ce96d4af4851b6378dc95421c1d974aeee2834b1
Gonçalo Pinto <63504894+gpe0@users.noreply.github.com>
2020-04-20 15:24:47 +0100

    Add ex012

---
+++ exercises/ex012/ex012/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ex012
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 7;
+            if (!(x < 7)) //Logical Operations | NOT - !
+            {
+                Console.WriteLine("Correct!");
+            }
+            else
+            {
+                Console.WriteLine("Wrong");
+            }
+            //Outputs "Correct!", because  the NOT operation change the logical value of the statement, if the statement was false, now it is true.
+        }
+    }
+}
