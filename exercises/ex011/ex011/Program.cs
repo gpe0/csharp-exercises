b7adc31e955808b42b6961c95837d5530b65b5a8
Gonçalo Pinto <63504894+gpe0@users.noreply.github.com>
2020-04-20 15:18:42 +0100

    Add ex011

---
+++ exercises/ex011/ex011/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ex011
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 7;
+            if (x < 7 || y > 2) //Logical Operations | OR - ||
+            {
+                Console.WriteLine("Correct!");
+            }
+            else
+            {
+                Console.WriteLine("Wrong");
+            }
+            //Outputs "Correct!", because  the result of an OR operation is only false when both statements are false.
+        }
+    }
+}
