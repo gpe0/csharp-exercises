e9e3299c7e4e9d4fb096c8534d994b9e04d9d3a5
Gonçalo Pinto <63504894+gpe0@users.noreply.github.com>
2020-04-21 14:44:15 +0100

    Add ex015

---
+++ exercises/ex015/ex015/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ex015
+{
+    class Program
+    {
+        //Methods!! || Methods have many advantages, including:
+        //- Reusable code.
+        //- Easy to test.
+        //- Modifications to a method do not affect the calling program.
+        //- One method can accept many different inputs.
+        static void SayHi()
+        {
+            Console.WriteLine("Hi!");
+        }
+        static int Sum(int x, int y)
+        {
+            return x + y;
+        }
+        static void Main(string[] args)
+        {
+            int result;
+            SayHi();
+            SayHi();
+            SayHi();
+            result = Sum(5, 6);
+            Console.WriteLine(result);
+            //Outputs 11
+        }
+    }
+}
