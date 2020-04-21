1ed9389647d226da643a1f89906404e01f950ce9
Gonçalo Pinto <63504894+gpe0@users.noreply.github.com>
2020-04-21 14:53:19 +0100

    Add ex016

---
+++ exercises/ex016/ex016/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ex016
+{
+    class Program
+    {
+        static void Print(string text) //"text" is a parameter
+        {
+            Console.WriteLine(text);
+        }
+        static void Person(string name, int age) // 2 parameters
+        {
+            Console.WriteLine("Person saved! \nName: {0} \nAge: {1}", name, age);
+        }
+        static void Main(string[] args)
+        {
+            Print("Hey!");
+            Person("Gonçalo", 18);
+        }
+    }
+}
