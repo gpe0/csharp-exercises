34b3a92baf44e78826e551d99c6141b5cf17628c
Gonçalo Pinto <63504894+gpe0@users.noreply.github.com>
2020-04-22 15:24:13 +0100

    Add ex018

---
+++ exercises/ex018/ex018/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ex018
+{
+    class Program
+    {
+        static void Print(string msg, int rep = 1)
+        {
+            for (int i = 0; i < rep; i++)
+            {
+                Console.WriteLine(msg);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Print(rep: 3, msg: "Hello, my is name is Gonçalo"); //Named parameters | As you can see, u don't need to put the parameters in order when you call the method!
+        }       // "name of the variable": "the value"
+    }
+}
