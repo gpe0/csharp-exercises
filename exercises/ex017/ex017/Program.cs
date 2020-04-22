f3e9ea7ace9ba8b502c438b448a76f1b96020af0
Gonçalo Pinto <63504894+gpe0@users.noreply.github.com>
2020-04-22 15:18:02 +0100

    Add ex017

---
+++ exercises/ex017/ex017/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ex017
+{
+    class Program
+    {
+        static void Pow(int x, int m = 2 /* optional parameter, if nothing is written, "m" will be equal to 2 */)
+        {
+            int sum = 1;
+            for (int i = 0; i < m; i++)
+            {
+                sum *= x;
+            }
+            Console.WriteLine(sum);
+        }
+        static void Main(string[] args)
+        {
+            Pow(5); // m = 2
+            Pow(3, 3); // m = 3
+        }
+    }
+}
