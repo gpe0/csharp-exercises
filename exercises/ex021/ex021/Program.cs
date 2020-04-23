3496693f04a1589e830c8825c5b795f9fb9f051b
Gonçalo Pinto <63504894+gpe0@users.noreply.github.com>
2020-04-23 14:49:50 +0100

    Add ex021

---
+++ exercises/ex021/ex021/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ex021
+{
+    class Program
+    {
+        // Recursion | A recursive method is a method that calls itself.
+        static int Factorial(int x)
+        {
+            if (x == 1)
+            {
+                return 1;
+            }
+            return x * Factorial(x - 1);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Factorial(5)); // Outputs 120, 5! = 5 x 4 x 3 x 2 x 1 = 120
+        }
+    }
+}
