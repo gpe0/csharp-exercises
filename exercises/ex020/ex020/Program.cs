21dd7baf606df342741ab120184c93974531c599
Gonçalo Pinto <63504894+gpe0@users.noreply.github.com>
2020-04-23 14:41:49 +0100

    Add ex020

---
+++ exercises/ex020/ex020/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ex020
+{
+    class Program
+    {
+        // Method overloading | same method name, diferent parameters' types
+        static void Print(int x)
+        {
+            Console.WriteLine("You wrote {0}, a integer number.", x);
+        }
+        static void Print(double x)
+        {
+            Console.WriteLine("You wrote {0}, a double number.", x);
+        }
+        static void Print(string x)
+        {
+            Console.WriteLine("You wrote '{0}', that is a string.", x);
+        }
+        static void Main(string[] args)
+        {
+            Print(1);
+            Print(1.23);
+            Print("Hello");
+        }
+    }
+}
