fb956c78858ab924ef7c4e2e1e41a3ed80e77870
Gonçalo Pinto <63504894+gpe0@users.noreply.github.com>
2020-04-24 14:24:18 +0100

    Add ex023

---
+++ exercises/ex023/ex023/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reflection.PortableExecutable;
+
+namespace ex023
+{
+    //Classes and Objects
+    class Person
+    {
+        public int age; // public is an access modifier that let you access the member from outside the class
+        public string name;
+        public void SayHi()
+        {
+            Console.WriteLine("Hi!");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            p1.age = 20;
+            p1.name = "John";
+            Console.WriteLine("name : {0}, age : {1}", p1.name, p1.age);
+            p1.SayHi();
+        }
+    }
+}
