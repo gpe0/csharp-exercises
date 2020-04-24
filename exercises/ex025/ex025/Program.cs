8c3de47b622979d8f20f6e998c19785bbc477c06
Gonçalo Pinto <63504894+gpe0@users.noreply.github.com>
2020-04-24 14:50:25 +0100

    Add ex025

---
+++ exercises/ex025/ex025/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ex025
+{
+    //Constructor is a special member method that is executed whenever a new object of that class is created.
+    class Person
+    {
+        private string name;
+        private int age;
+        public Person(string nm, int a) //Here i'm saying that whenever a nre object is created, the user need to input the person's name and age.
+        {
+            name = nm;
+            age = a;
+        }
+        public void GetName()
+        {
+            Console.WriteLine("Name: {0}", name);
+        }
+        public void GetAge()
+        {
+            Console.WriteLine("Age: {0}", age);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person("John", 20);
+            p1.GetName();
+            p1.GetAge();
+        }
+    }
+}
