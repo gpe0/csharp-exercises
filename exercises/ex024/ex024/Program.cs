6e3702429ab82bbf1d20cb7b729857b14d74cc85
Gonçalo Pinto <63504894+gpe0@users.noreply.github.com>
2020-04-24 14:36:34 +0100

    Add ex024

---
+++ exercises/ex024/ex024/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ex024
+{
+    // Encapsulation || used to hide information, protecting the class
+    class BankAccount
+    {
+        private double balance = 0; // A private member can't be accessed from outside the class
+        public void Deposit(double amount)
+        {
+            balance += amount;
+            Console.WriteLine("{0}$ added to your account.", amount);
+        }
+        public void GetBalance()
+        {
+            Console.WriteLine("Account value: {0}$", balance);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount a1 = new BankAccount();
+            // Now you can't access your account's balance directly because it is protected. So we use public methods.
+            a1.GetBalance();
+            a1.Deposit(50.32);
+            a1.GetBalance();
+        }
+    }
+}
