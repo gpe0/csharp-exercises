d279482c1c7ea7c542ca669011ce93abc700358a
Gonçalo Pinto <63504894+gpe0@users.noreply.github.com>
2020-04-25 14:01:28 +0100

    Add ex026

---
+++ exercises/ex026/ex026/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ex026
+{
+    class Person
+    {
+        private string name;
+        private int age;
+        public string Name //This is a property
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        //You can use logic in your properties
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value > 0)
+                {
+                    age = value;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Age!");
+                }
+            }
+        }
+    }
+    class Dog
+    {
+        //Auto-implemented properties
+        public int Age { get; set; } //With the auto-implemented property you don't need to crate a private variable called "age", that is created automatically.
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            p1.Name = "John";
+            Console.WriteLine(p1.Name);
+            p1.Age = -2;
+            p1.Age = 20;
+            Console.WriteLine(p1.Age);
+            Dog d1 = new Dog();
+            d1.Age = 3;
+            Console.WriteLine(d1.Age);
+        }
+    }
+}
