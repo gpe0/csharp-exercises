ea66660ea5a57b70dba04609addd1724ef0380fb
Gonçalo Pinto <63504894+gpe0@users.noreply.github.com>
2020-04-25 14:23:01 +0100

    Add ex027

---
+++ exercises/ex027/ex027/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ex027
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Arrays | They are objects used to store multiple values
+            int[] a1 = new int[5]; // i created a array that stores 5 integer values.
+            a1[0] = 3; //the first index is 0, not 1.
+            Console.WriteLine(a1[0]);
+            // you can create arrays and setting values with curly brackets.
+            // int[] a2 = new int[3] {3, 4, 2}
+            // or
+            // int[] a2 = new int[] {3, 4, 2}
+            // or 
+            int[] a2 = { 3, 4, 2 };
+            Console.WriteLine(a2[2]); // All meaning the same.
+        }
+    }
+}
