484d430f680fe9b05435c184ace8e315e07c810b
Gonçalo Pinto <63504894+gpe0@users.noreply.github.com>
2020-04-26 14:54:32 +0100

    Add ex029

---
+++ exercises/ex029/ex029/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ex029
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Multi dimensional arrays
+            int[,] a = new int[3, 2]; // This will create 3 rows and 2 columns
+                                      //        Column 1    Column 2
+                                      // Row 1   [0, 0]      [0, 1]
+                                      // Row 2   [1, 0]      [1, 1]
+                                      // Row 3   [2, 0]      [2, 1]
+            int[,] arr = { { 2, 4 }, { 0, 2 }, { 3, 7 } };
+            Console.WriteLine(arr[2, 0]); //Outputs 3
+        
+        }
+    }
+}
