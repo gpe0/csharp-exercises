a6d3344e05eb921ec4bfaf9960349910d37a64bd
Gonçalo Pinto <63504894+gpe0@users.noreply.github.com>
2020-04-26 15:12:43 +0100

    Add ex031

---
+++ exercises/ex031/ex031/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace ex031
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Some properties and methods of the array. 
+            int[] arr = { 4, 65, 1, 20, 5 };
+            Console.WriteLine(arr.Length); // Outputs the number of elements (5)
+            Console.WriteLine(arr.Rank); // Outputs the dimensions of the array (1)
+            Console.WriteLine(arr.Min()); // Outputs the minimum number of the array (1)
+            Console.WriteLine(arr.Max()); // Outputs the maximum number of the array (65)
+            Console.WriteLine(arr.Sum()); // Outputs the sum of the array (95)
+        }
+    }
+}
