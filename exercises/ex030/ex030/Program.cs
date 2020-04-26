8bc29e2abf08f82b85c940b0ff91ff8e491e0a4b
Gonçalo Pinto <63504894+gpe0@users.noreply.github.com>
2020-04-26 15:01:20 +0100

    Add ex030

---
+++ exercises/ex030/ex030/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ex030
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Jagged arrays | Array of arrays
+            int[][] a = new int[][] 
+            {
+                new int[] {4, 3, 2},
+                new int[] {2, 9, 6},
+                new int[] {10, 459, 67} // I created an array of 3 arrays
+            };
+            Console.WriteLine(a[2][1]); // Outputs 459
+        }
+    }
+}
