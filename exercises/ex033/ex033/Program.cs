f4749b4f830a97b28bbd3352323cc4651c649bd2
Gonçalo Pinto <63504894+gpe0@users.noreply.github.com>
2020-04-27 14:46:15 +0100

    Add ex033

---
+++ exercises/ex033/ex033/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ex033
+{
+    //Static keyword | the properties and methods belong to the class itself, not to the objects
+    class Dog
+    {
+        static public int dogs = 0;
+        public Dog()
+        {
+            dogs++;
+        }
+        public static void Bark()
+        {
+            Console.WriteLine("Woof!");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog d1 = new Dog();
+            Dog d2 = new Dog();
+            Dog.Bark(); // As you can see, we are calling the class, not the object
+            Console.WriteLine(Dog.dogs);
+        }
+    }
+}
