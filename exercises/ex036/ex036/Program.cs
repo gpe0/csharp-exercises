fd64de1daf2b4a58936de2a81729bcb7a1e3daa6
Gonçalo Pinto <63504894+gpe0@users.noreply.github.com>
2020-04-28 14:48:26 +0100

    Add ex036

---
+++ exercises/ex036/ex036/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ex036
+{
+    class Program
+    {
+        //Arrays in Classes
+        class Clients
+        {
+            private string[] names = new string[2];
+            public string this[int index]
+            {
+                get { return names[index]; }
+                set { names[index] = value; }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Clients c = new Clients();
+            c[0] = "Jonh";
+            c[1] = "Michael";
+            Console.WriteLine(c[1]);
+        }
+    }
+}
