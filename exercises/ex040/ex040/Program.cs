878a1eb9cd8594ecddb88cd6d1ddf38d13e36f7b
Gonçalo Pinto <63504894+gpe0@users.noreply.github.com>
2020-04-29 15:16:16 +0100

    Add ex040

---
+++ exercises/ex040/ex040/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ex040
+{
+    //Constructors in base and derived classes
+    class Animal
+    {
+        public Animal()
+        {
+            Console.WriteLine("Animal is created");
+        }
+    }
+    class Dog : Animal
+    {
+        public Dog()
+        {
+            Console.WriteLine("Dog is created");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog d1 = new Dog(); //As you an see, both constructors are called
+            // Note that the base class constructor is called first and the derived class constructor is called next.
+        }
+    }
+}
