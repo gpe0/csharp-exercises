a2a60ea7d8ed0e21a47df955bc7331a6fb7563bc
Gonçalo Pinto <63504894+gpe0@users.noreply.github.com>
2020-04-29 15:04:46 +0100

    Add ex039

---
+++ exercises/ex039/ex039/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ex039
+{
+    //protected and sealed keywords
+    class Person
+    {
+        protected string Name { get; set; } //This variable is accessed in this class and it can be accessed in the derived classes.
+    }
+    class Student : Person
+    {
+        public Student(string name)
+        {
+            Name = name;
+        }
+        public void GetName()
+        {
+            Console.WriteLine(this.Name);
+        }
+    }
+    sealed class Box //This class prevented other classes from inheriting it.
+    {
+        private double width;
+        private double height;
+    }
+    // " class Cube : Box " will generate an error.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student("John");
+            s1.GetName();
+            //" s1.Name " will generate an error.
+        }
+    }
+}
