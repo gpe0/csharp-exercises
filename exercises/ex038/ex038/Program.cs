160f8f86b04fb55896ed4b1c13736a6d43b5784c
Gonçalo Pinto <63504894+gpe0@users.noreply.github.com>
2020-04-29 14:55:33 +0100

    Add ex038

---
+++ exercises/ex038/ex038/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ex038
+{
+    //Inheritance
+    class Animal //This is a base class
+    {
+        public int Legs { get; set; }
+        public void Info()
+        {
+            Console.WriteLine("Animal class");
+        }
+    }
+    class Dog : Animal //This is a derived class
+    {
+        public Dog()
+        {
+            Legs = 4;
+        }
+        public void Bark()
+        {
+            Console.WriteLine("Woof!");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal a = new Animal(); //This animal can't bark, because it is not from Dog class
+            Dog d = new Dog(); //But this one can use "Info()" because it is an instance of a derived class of Animal class.
+            a.Info();
+            d.Bark();
+            d.Info();
+
+        }
+    }
+}
