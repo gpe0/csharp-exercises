9f8e103cfabac73a528b02747c981e17b97c1ca7
Gonçalo Pinto <63504894+gpe0@users.noreply.github.com>
2020-04-30 14:16:41 +0100

    Add ex042

---
+++ exercises/ex042/ex042/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ex042
+{
+    //Abstract methods and classes
+    abstract class Animal //Abstract classes can only have abstract methods. They can't be instantiated.
+    {
+        public abstract void Sound();
+    }
+    class Dog : Animal
+    {
+        public override void Sound()
+        {
+            Console.WriteLine("Woof");
+        }
+    }
+    class Cat : Animal
+    {
+        public override void Sound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog d1 = new Dog();
+            Cat c1 = new Cat();
+            d1.Sound();
+            c1.Sound();
+        }
+    }
+}
