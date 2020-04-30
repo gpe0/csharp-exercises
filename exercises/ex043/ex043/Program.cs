5a7615a7de0d21d5b1125476528650c2bfae5236
Gonçalo Pinto <63504894+gpe0@users.noreply.github.com>
2020-04-30 14:29:40 +0100

    Add ex043

---
+++ exercises/ex043/ex043/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace ex043
+{
+    //Interfaces | An interface is a completely abstract class, which contains only abstract members.
+    interface IAnimal // A class can inherit from just one base class, but it can implement multiple interfaces, that's why it is cool.
+    {
+        void Sound();
+    }
+    class Dog : IAnimal
+    {
+        public void Sound()
+        {
+            Console.WriteLine("Woof");
+        }
+    }
+    class Cat : IAnimal
+    {
+        public void Sound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog d1 = new Dog();
+            Cat c1 = new Cat();
+            d1.Sound();
+            c1.Sound();
+        }
+    }
+}
