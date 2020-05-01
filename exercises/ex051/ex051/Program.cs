3a0838086941ca4238686a9364f5e5ed1389f6c1
Gonçalo Pinto <63504894+gpe0@users.noreply.github.com>
2020-05-01 21:44:15 +0100

    Add ex051

---
+++ exercises/ex051/ex051/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ex051
+{
+    //Generic classes | Really similar to generic methods.
+    class ArrayCreator<T>
+    {
+        private int ind = 0;
+        private T[] arr;
+        public ArrayCreator(int x)
+        {
+            arr = new T[x];
+        }
+        public void Add(T x)
+        {
+            arr[ind++] = x;
+        }
+        public void Get(int x)
+        {
+            try
+            {
+                Console.WriteLine(arr[x]);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("ERROR");
+            }
+            
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayCreator<string> a = new ArrayCreator<string>(2);
+            a.Add("Hello");
+            a.Add("World");
+            a.Get(1);
+            a.Get(2);
+        }
+    }
+}
