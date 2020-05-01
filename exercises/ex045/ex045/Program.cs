83089e6cae0f5106dcddaacd93f2fb567c39044a
Gonçalo Pinto <63504894+gpe0@users.noreply.github.com>
2020-05-01 16:09:05 +0100

    Add ex045

---
+++ exercises/ex045/ex045/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ex045
+{
+    //Structs | They are like classes, but you should use them to represent a simple set of data.
+    struct Person
+    {
+        public string name;
+        public int age;
+        public void SayHi()
+        {
+            Console.WriteLine("Hi!");
+        }
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //You don't need to use the "new" keyword but when you have a constructor you must do it.
+            Person p1 = new Person("John", 20);
+            Console.WriteLine(p1.age);
+            p1.SayHi();
+
+        }
+    }
+}
