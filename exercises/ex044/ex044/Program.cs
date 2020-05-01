fc4dc4b319c7b4533ff6864c7eff373067ab4ffb
Gonçalo Pinto <63504894+gpe0@users.noreply.github.com>
2020-05-01 15:57:03 +0100

    Add ex044

---
+++ exercises/ex044/ex044/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ex044
+{
+    //Nested classes
+    class Car
+    {
+        private int wheels = 4;
+        public Car()
+        {
+            Motor m = new Motor();
+        }
+        public void GetWheels()
+        {
+            Console.WriteLine("Your car has {0} wheels.", wheels);
+        }
+        public class Motor // The Motor class is nested in Car class
+        {
+            public void TurnOn()
+            {
+                Console.WriteLine("The car is now ON");
+            }
+        }
+    }
+       
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car c1 = new Car(); //This is the car
+            c1.GetWheels();
+            Car.Motor m1 = new Car.Motor(); //This is the actual motor
+            m1.TurnOn();
+        }
+    }
+}
