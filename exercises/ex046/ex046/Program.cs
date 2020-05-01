c3172f923dff90ede2b04850b98f89b4d5a2559b
Gonçalo Pinto <63504894+gpe0@users.noreply.github.com>
2020-05-01 16:21:23 +0100

    Add ex046

---
+++ exercises/ex046/ex046/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ex046
+{
+    //Enums | The enum keyword is used to declare an enumeration. | A type that consists of a set of named constants called the enumerator list.
+    //By default, the first enumerator has the value 0, and the value of each successive enumerator is increased by 1.
+    enum Days { Sun, Mon, Tue, Wed, Thu, Fri, Sat}
+    //           0    1    2    3    4    5    6
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = (int)Days.Mon;
+            Console.WriteLine(x); //Outputs 1
+        }
+    }
+}
